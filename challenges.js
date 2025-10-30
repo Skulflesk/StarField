bbfc2e69f35532f9ba656cd16036b0b6f423b88f
Skulflesk <rjseese@email.neit.edu>
2025-10-30 10:30:52 -0400

    Update challenges.js

---
+++ challenges.js
@@ -0,0 +1,14 @@
+let canvas = document.getElementById("myCanvas");
+let pecil = document.getContext("2d");
+
+function gameLoop() {
+
+
+        //draw background
+        //draw stars
+        //move stars
+        //recycle/restart stars
+
+}
+
+setInterval(gameLoop, 50);
